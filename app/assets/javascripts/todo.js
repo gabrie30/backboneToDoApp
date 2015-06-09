c1510e84a1bf94c7a6b8d215909a48e3a49e9ab6
gabrie30 <jgabriels30@gmail.com>
2015-06-08 19:17:49 -0700

    added initialize into app

---
+++ app/assets/javascripts/todo.js
@@ -0,0 +1,14 @@
+window.Todo = {
+  Models: {},
+  Views: {},
+  Routes: {},
+
+  initialize: function() {
+    alert("WELCOME TO DO");
+  }
+
+};
+
+$( function() {
+  window.Todo.initialize();
+});
